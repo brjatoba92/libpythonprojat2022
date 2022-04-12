0862ee7353c535394367a383a040bba3c8a9ab12
brjatoba92 <brjatoba92@gmail.com>
2022-04-12 18:38:31 -0300

    Implementado Injeção de Dependências
    close #48

--- libpythonprojat2022/tests/test_spam/test_envio_para_base_de_usuario.py
+++ libpythonprojat2022/tests/test_spam/test_envio_para_base_de_usuario.py
@@ -20,7 +20,7 @@ from libpythonprojat2022.spam.modelos import Usuario
 def test_qde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'renzo@python.pro.br',
@@ -28,3 +28,33 @@ def test_qde_de_spam(sessao, usuarios):
         'Confira os modulos fantasticos'
     )
     assert len(usuarios) == enviador.qtd_email_enviados
+
+
+class EnviadorMock(Enviador):
+
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Renzo', email='renzo@python.pro.br')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'jorge@python.pro.br',
+        'Curso Python Pro',
+        'Confira os modulos fantasticos'
+    )
+    assert enviador.parametros_de_envio == (
+        'jorge@python.pro.br',
+        'renzo@python.pro.br',
+        'Curso Python Pro',
+        'Confira os modulos fantasticos'
+    )
