823d6b5dff48391e33ffe7db1d7e36c33685d74c
brjatoba92 <brjatoba92@gmail.com>
2022-04-13 20:37:24 -0300

    Implementado Biblioteca Pytest-Mock
    close #56

--- libpythonprojat2022/tests/test_spam/test_gihub_api.py
+++ libpythonprojat2022/tests/test_spam/test_gihub_api.py
@@ -6,21 +6,20 @@ from libpythonprojat2022 import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/69490377?v=4'
     resp_mock.json.return_value = {'login': 'brjatoba92', 'id': 69490377,
-                                   'avatar_url': avatar_url,
+                                   'avatar_url': url,
                                    }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('libpythonprojat2022.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
 
 
 def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('brjatoba92')
-    assert url == url
+    assert avatar_url == url
 
 
 def test_buscar_avatar_integracao():
